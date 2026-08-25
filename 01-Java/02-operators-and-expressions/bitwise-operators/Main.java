ce51c8895d246f0a22f2612e5e8698c5d4c3af6d
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-08-25 17:00:06 +0530

    bitwise operator add

---
+++ 01-Java/02-operators-and-expressions/bitwise-operators/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Bit-wise operators AND, OR, XOR
+        int x = 10, y = 6, z;
+        z = x&y; //& means and
+        System.out.println(z); //2 how??
+
+        /*
+        10 -> 0 0 0 0 1 0 1 0
+        6  -> 0 0 0 0 0 1 1 0
+        --------------------------
+        z  -> 0 0 0 0 0 0 1 0 //2
+        */
+    }
+}
