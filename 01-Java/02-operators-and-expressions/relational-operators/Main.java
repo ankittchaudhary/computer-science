f72fdae2502d01240dc27cf0676e9c0d6b833b0d
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-09-02 12:54:39 +0530

    relational operator add

---
+++ 01-Java/02-operators-and-expressions/relational-operators/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        //Relational Operators
+        /*
+        - Used for comparing the values
+
+        <    less than
+        <=   less than equal to
+        >    greater than
+        >=   greater than equal to
+        ==   is equal to
+        !=   not equal to
+
+        */
+
+        int a =5;
+        int b =10;
+        System.out.println(a<b); //true
+        System.out.println(a<=b); //true 
+        System.out.println(a>b); //false
+        System.out.println(a>=b); //false
+        System.out.println(a==b); //false
+        System.out.println(a!=b); //true
+    }
+}
