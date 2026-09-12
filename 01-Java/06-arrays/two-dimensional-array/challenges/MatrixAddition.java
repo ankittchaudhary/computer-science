944270b845fd99a7ace746de50898a1e25462bcf
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-09-12 15:24:39 +0530

    2-d array challenge: Matrix Addition

---
+++ 01-Java/06-arrays/two-dimensional-array/challenges/MatrixAddition.java
@@ -0,0 +1,33 @@
+package challenges;
+public class MatrixAddition {
+    public static void main(String[] args) {
+        
+
+        //1. Adding two matrices
+        // require same dimension for adding or substracting matrices
+
+
+        int [][] A = {{2, 3, 5}, {6, 7, 9}, {4, 3, 5}}; //3x3
+        int [][] B = {{1, 5, 2}, {6, 8, 4}, {3, 9, 7}}; //3x3
+        
+        int [][] C = new int [A.length][A.length]; //sum matrix 3x3
+
+        for(int i = 0; i<A.length; i++){
+            for(int j=0; j<A[0].length; j++){             //why A[0] and not B[0]: because both are of same size so does not matter, and its not jagged either
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        print(C);
+
+    }
+    static void print(int arr[][]){
+        for (int[] row : arr) {
+            for (int elem : row) {
+                System.out.print(elem+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
