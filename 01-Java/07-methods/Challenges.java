da507babe0bffd59d284ec73d866d3336f6422b4
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-09-15 14:26:00 +0530

    methods: challenges add

--- 01-Java/07-methods/Challenges.java
+++ 01-Java/07-methods/Challenges.java
@@ -1,9 +1,17 @@
 public class Challenges {
     public static void main(String[] args) {
         //1. Check if number is prime
-        int num =5;
-        System.out.println(isPrime(num));
+        // int num =5;
+        // System.out.println(isPrime(num));
 
+        //2. gcd
+        // int num1 = 91;
+        // int num2 = 13;
+        // System.out.println(gcd(num1, num2));
+
+        //3. return max from int array
+        int nums[] = {1, 3, 9, 11, 10, 17, 0};
+        System.out.println(max(nums));
     }
 
     static boolean isPrime(int num){
@@ -14,5 +22,26 @@ public class Challenges {
             if(num%2==0) return false;
         }
         return true;
+    }
+
+    static int gcd(int num1, int num2){
+        while(num1!=num2){
+            if(num1>num2){
+                num1=num1-num2;
+            } else {
+                num2 = num2 -num1;
+            }            
+        }
+        return num1;
+    }
+
+    static int max(int[] nums){
+        int max = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+        return max;
     }
 }
